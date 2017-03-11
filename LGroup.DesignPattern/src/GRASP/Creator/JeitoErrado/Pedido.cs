2fb812e976291f2a2b54e101bc758ddd24569fae
Luisfdc <luisfd.campos@gmail.com>
2017-03-11 12:57:36 -0300

    Creator

---
+++ LGroup.DesignPattern/src/GRASP/Creator/JeitoErrado/Pedido.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creator.JeitoErrado
+{
+    public class Pedido
+    {
+        public List<ItemPedido> Itens { get; set; }
+        
+        public void AddItemPedido(int idProduto, int qtd)
+        {
+            var item = new ItemPedido(idProduto, qtd);
+            Itens.Add(item);
+        }
+
+        public decimal GetTotal()
+        {
+            var total = 0M;
+
+            foreach (var item in Itens)
+            {
+                total += item.TotalParcial();
+            }
+
+            return total;
+        }
+    }
+}
